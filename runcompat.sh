abc9e5f1b8ae5172a06e8567f79ea3b84fe8904a
Siavash Mirarab <smirarab@gmail.com>
2014-12-16 14:53:41 -0600

    First commit (as in Science paper)

---
+++ runcompat.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+test $# == 5 || echo USAGE: directory filename gene versus support
+
+dir=$1
+f=$2
+x=$3
+all=$4
+supp=$5
+
+if [ "$supp" == "" ]; then 
+  temp=$dir/$x/$f
+else
+  temp=`mktemp /tmp/temp.XXXXXX`
+  python $BIN_HOME/remove_edges_from_tree.py $dir/$x/$f $supp $temp -strip-both
+fi
+
+$BIN_HOME/compareTrees.compatibility $temp $all |paste -d " " $all.order - | sed -e "s/"$dir."/"$x" /g" -e "s/\/[^ ]* / /g"
